de9436cb35d4fc3d75ddc6b30d4dde34cb619322
Frank Miranda <frank.miranda12@hotmail.com>
2026-05-30 14:47:36 -0300

    criado uma rota para conclusão de cursos

--- src/routes/cursos.routes.js
+++ src/routes/cursos.routes.js
@@ -2,6 +2,7 @@ const express = require('express');
 const router = express.Router();
 const { authMiddleware, adminMiddleware } = require('../middlewares/auth.middleware');
 const { listarCursos, listarTodosCursos, enviarCurso, deletarCurso } = require('../controllers/cursos.controller');
+const cursosController = require('../controllers/cursos.controller');
 
 // Funcionário vê seus cursos
 router.get('/', authMiddleware, listarCursos);
@@ -10,5 +11,5 @@ router.get('/', authMiddleware, listarCursos);
 router.get('/todos', authMiddleware, adminMiddleware, listarTodosCursos);
 router.post('/', authMiddleware, adminMiddleware, enviarCurso);
 router.delete('/:id', authMiddleware, adminMiddleware, deletarCurso);
-
+router.patch('/:id/concluir', authMiddleware, cursosController.concluirCurso);
 module.exports = router;
